280e74420e0b8ec25b6bcefe628a3bb8e3d66244
Artur Chesnokov <artur.chesnokov@gmail.com>
2020-04-15 23:18:15 +0300

    context processor

--- src/user_profile/forms.py
+++ src/user_profile/forms.py
@@ -3,9 +3,9 @@ from django.forms import DateInput
 from user_profile.models import User
 
 
+# 6. forms-widgets - assign calendar widget to "date of birth" field.
 # 7. forms-model-extra - (edit profile form has been done with forms.ModelForm
 class CustomUserChangeForm(forms.ModelForm):
-    # 6. forms-widgets - assign calendar widget to "date of birth" field.
     date_of_birth = forms.DateField(
         widget=DateInput(attrs={
             'type': 'date',
